a3a5fff978c220021633950c85399ab585fce1f2
AaronWongNSC <31226232+AaronWongNSC@users.noreply.github.com>
2017-10-08 20:28:04 -0700

    Create pygame-FlappyBox-ControlsOnly.py

---
+++ pygame-FlappyBox-ControlsOnly.py
@@ -0,0 +1,92 @@
+import pygame
+import random
+import math
+
+# Constants
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (255, 0, 255)
+CYAN = (0, 255, 255)
+
+class FlappyBox(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((20,20))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+
+        self.rect.bottom = HEIGHT/2
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+
+        self.y_vel = 0
+
+    def update(self):
+        self.rect.y += self.y_vel
+        self.y_vel += 1
+        
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keystate[pygame.K_RIGHT]:
+            self.rect.x += 5
+
+        print(self.rect.left, self.rect.right, self.rect.bottom, self.y_vel)
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+            self.y_vel = 0
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.y_vel = int(-0.5*self.y_vel)
+
+# Initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('My Game')
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+
+player = FlappyBox()
+
+all_sprites.add(player)
+
+# Game loop
+running = True
+while running:
+    # Control game speed
+    clock.tick(FPS)
+    
+    # Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.y_vel -= 10
+    
+    # Update
+    all_sprites.update()
+    
+    # Render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    
+pygame.quit()
+
